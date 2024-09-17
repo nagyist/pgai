3c1e7657dc85a94b87be11633e7d19eea954ee42
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-09-17 15:23:44 -0300

    Anthropic use_tool example

--- sql/idempotent/003-anthropic.sql
+++ sql/idempotent/003-anthropic.sql
@@ -2,7 +2,7 @@
 -------------------------------------------------------------------------------
 -- anthropic_generate
 -- https://docs.anthropic.com/en/api/messages
-create or replace function ai.anthropic_generate
+CREATE OR REPLACE FUNCTION ai.anthropic_generate
 ( _model text
 , _messages jsonb
 , _max_tokens int default 1024
@@ -18,28 +18,29 @@ create or replace function ai.anthropic_generate
 , _tools jsonb default null
 , _top_k int default null
 , _top_p float8 default null
-) returns jsonb
-as $python$
+) RETURNS jsonb
+AS $python$
     #ADD-PYTHON-LIB-DIR
     import ai.anthropic
+    import json
+
     client = ai.anthropic.make_client(plpy, api_key=_api_key, base_url=_base_url, timeout=_timeout, max_retries=_max_retries)
 
-    import json
     _messages_1 = json.loads(_messages)
 
     args = {}
     if _system is not None:
         args["system"] = _system
     if _user_id is not None:
-        args["metadata"] = {"user_id", _user_id}
+        args["metadata"] = {"user_id": _user_id}
     if _stop_sequences is not None:
         args["stop_sequences"] = _stop_sequences
     if _temperature is not None:
         args["temperature"] = _temperature
     if _tool_choice is not None:
-        args["tool_choice"] = json.dumps(_tool_choice)
+        args["tool_choice"] = json.loads(_tool_choice)
     if _tools is not None:
-        args["tools"] = json.dumps(_tools)
+        args["tools"] = json.loads(_tools)
     if _top_k is not None:
         args["top_k"] = _top_k
     if _top_p is not None:
@@ -48,6 +49,5 @@ as $python$
     message = client.messages.create(model=_model, messages=_messages_1, max_tokens=_max_tokens, **args)
     return message.to_json()
 $python$
-language plpython3u volatile parallel safe security invoker
-set search_path to pg_catalog, pg_temp
-;
+LANGUAGE plpython3u VOLATILE PARALLEL SAFE SECURITY INVOKER
+set search_path to pg_catalog, pg_temp ;
